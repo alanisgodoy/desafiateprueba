6731758b79d63fb027501c377b9bc881d8390239
alanisgodoy <alanisgodoy90@gmail.com>
2024-08-28 21:38:12 -0300

    Add files via upload

---
+++ products.js
@@ -0,0 +1,28 @@
+// Function to create product boxes
+let showProducts = (productsArray) =>{
+    let htmlProductsToAppend = "";
+    productsArray.forEach(product => {
+        htmlProductsToAppend += `
+            <div class="product">
+                <img src= "${product.image}" alt="${product.name}">
+                <h2>${product.name}</h2>
+                <p class="description">${product.description}</p>
+                <p class="price">${product.currency} ${product.cost}</p>
+                <p class="sold-quantity">Cantidad de vendidos:${product.soldCount}</p>
+            </div>
+        `
+    })
+    document.getElementById('product-container').innerHTML = htmlProductsToAppend;
+}
+
+//function that will be executed when the document completely is fully loaded 
+
+document.addEventListener('DOMContentLoaded',(e)=>{
+   getJSONData(PRODUCTS_URL+101+EXT_TYPE)
+        .then(object =>{
+            if(object.status === 'ok'){
+              let productsArray = object.data.products;
+            showProducts(productsArray);  
+            };
+        } );
+});
